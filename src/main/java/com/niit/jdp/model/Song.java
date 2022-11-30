c2e85205ad862a5a225c88ed5d708f13d876d925
Ketki Keni <sketki220@gmail.com>
2022-11-30 17:07:09 +0530

    added private fields to the Song type

--- src/main/java/com/niit/jdp/model/Song.java
+++ src/main/java/com/niit/jdp/model/Song.java
@@ -7,4 +7,9 @@
 package com.niit.jdp.model;
 
 public class Song {
+    private int serialNumber;
+    private String name;
+    private double duration;
+    private String genre;
+    private String artistName;
 }
