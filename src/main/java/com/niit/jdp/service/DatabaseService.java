8b03f3d7a280503a49f1f1dec6f68988ffe4d26e
Ketki Keni <sketki220@gmail.com>
2022-11-30 17:37:16 +0530

    added a no-arguments constructor in the DatabaseService class

--- src/main/java/com/niit/jdp/service/DatabaseService.java
+++ src/main/java/com/niit/jdp/service/DatabaseService.java
@@ -9,9 +9,13 @@ package com.niit.jdp.service;
 import java.sql.Connection;
 
 public class DatabaseService {
-    private static final String URL = "jdbc:mysql://localhost:3306/school";
+    private static final String URL = "jdbc:mysql://localhost:3306/jukebox";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "Kets@8452037892";
     Connection connection;
+
+    public DatabaseService() {
+        connection = null;
+    }
 
 }
