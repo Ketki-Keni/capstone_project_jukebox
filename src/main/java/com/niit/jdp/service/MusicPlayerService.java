4ee72c50e5a3cebb6b6491b6fc5331c88db26c1b
Ketki Keni <sketki220@gmail.com>
2022-12-01 11:45:29 +0530

    completed play() method of the MusicPlayerService class

--- src/main/java/com/niit/jdp/service/MusicPlayerService.java
+++ src/main/java/com/niit/jdp/service/MusicPlayerService.java
@@ -6,9 +6,34 @@
 
 package com.niit.jdp.service;
 
-public class MusicPlayerService {
-    public void play() {
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 
+public class MusicPlayerService {
+    public void play(String songPath) {
+        //creating a file object to represent the song file
+        File songFile = new File(songPath);
+        try {
+            //creating an object of AudioInputStream class
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(songFile);
+            //getting a clip object from the AudioSystem
+            Clip clip = AudioSystem.getClip();
+            //open the clip and load the audio input stream
+            clip.open(audioInputStream);
+            //setting a loop for the sound file
+            clip.loop(clip.LOOP_CONTINUOUSLY);
+            //start the clip
+            clip.start();
+            //getting the exact length of the song in milliseconds
+            long songlength = clip.getMicrosecondLength() / 1000L;
+            //Pause the current thread for the time the song is playing
+            Thread.sleep(songlength);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException exception) {
+            exception.printStackTrace();
+        } catch (InterruptedException e) {
+            System.err.println("The song was interrupted");
+        }
     }
 
     public void stop() {
