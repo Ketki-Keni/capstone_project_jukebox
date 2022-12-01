df985c8d4926961a1b1068e46725c3c13a7f24b0
Ketki Keni <sketki220@gmail.com>
2022-12-01 15:33:20 +0530

    called displaySongByName method of songRepository class inside main()

--- src/main/java/com/niit/jdp/Main.java
+++ src/main/java/com/niit/jdp/Main.java
@@ -1,20 +1,30 @@
 package com.niit.jdp;
 
+import com.niit.jdp.model.Song;
 import com.niit.jdp.repository.SongRepository;
 import com.niit.jdp.service.MusicPlayerService;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        MusicPlayerService musicPlayerService = new MusicPlayerService();
-        musicPlayerService.play("src/main/resources/songs/01-Genshin-Impact-Main-Theme.wav");
-
         try {
             SongRepository songRepository = new SongRepository();
-            songRepository.displayAllSongs();
+            List<Song> allsongs = songRepository.displayAllSongs();
+            ;
+            for (Song song : allsongs) {
+                System.out.println(song);
+            }
+
+            System.out.println("Song based on name");
+            System.out.println(songRepository.displaySongByName("Liyue"));
+
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
+
+        MusicPlayerService musicPlayerService = new MusicPlayerService();
+        //musicPlayerService.play("src/main/resources/songs/01-Genshin-Impact-Main-Theme.wav");
     }
 }
