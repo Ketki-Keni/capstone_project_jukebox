f5bf5a79987f3b40adc2736b09e44c05ec646e28
Ketki Keni <sketki220@gmail.com>
2022-12-01 14:53:34 +0530

    called displayAllSongs method of songRepository class inside main()

--- src/main/java/com/niit/jdp/Main.java
+++ src/main/java/com/niit/jdp/Main.java
@@ -1,10 +1,20 @@
 package com.niit.jdp;
 
+import com.niit.jdp.repository.SongRepository;
 import com.niit.jdp.service.MusicPlayerService;
+
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
         MusicPlayerService musicPlayerService = new MusicPlayerService();
         musicPlayerService.play("src/main/resources/songs/01-Genshin-Impact-Main-Theme.wav");
+
+        try {
+            SongRepository songRepository = new SongRepository();
+            songRepository.displayAllSongs();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 }
