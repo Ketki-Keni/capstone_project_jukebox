7624a5ede34f6af1208bca579d3db0979c6f7b4a
Ketki Keni <sketki220@gmail.com>
2022-12-01 15:38:23 +0530

    added new fields in the Song class

--- src/main/java/com/niit/jdp/model/Song.java
+++ src/main/java/com/niit/jdp/model/Song.java
@@ -14,16 +14,20 @@ public class Song {
     private double duration;
     private String genre;
     private String artistName;
+    private String album;
+    private String songPath;
+
 
     public Song() {
     }
 
-    public Song(int serialNumber, String name, double duration, String genre, String artistName) {
+    public Song(int serialNumber, String name, double duration, String genre, String artistName, String album) {
         this.serialNumber = serialNumber;
         this.name = name;
         this.duration = duration;
         this.genre = genre;
         this.artistName = artistName;
+        this.album = album;
     }
 
     public int getSerialNumber() {
@@ -66,6 +70,22 @@ public class Song {
         this.artistName = artistName;
     }
 
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getSongPath() {
+        return songPath;
+    }
+
+    public void setSongPath(String songPath) {
+        this.songPath = songPath;
+    }
+
     @Override
     public String toString() {
         return "Song{" +
@@ -74,6 +94,8 @@ public class Song {
                 ", duration=" + duration +
                 ", genre='" + genre + '\'' +
                 ", artistName='" + artistName + '\'' +
+                ", album='" + album + '\'' +
+                ", songPath='" + songPath + '\'' +
                 '}';
     }
 
@@ -82,11 +104,11 @@ public class Song {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Song song = (Song) o;
-        return getSerialNumber() == song.getSerialNumber() && Double.compare(song.getDuration(), getDuration()) == 0 && Objects.equals(getName(), song.getName()) && Objects.equals(getGenre(), song.getGenre()) && Objects.equals(getArtistName(), song.getArtistName());
+        return getSerialNumber() == song.getSerialNumber() && Double.compare(song.getDuration(), getDuration()) == 0 && Objects.equals(getName(), song.getName()) && Objects.equals(getGenre(), song.getGenre()) && Objects.equals(getArtistName(), song.getArtistName()) && Objects.equals(getAlbum(), song.getAlbum()) && Objects.equals(getSongPath(), song.getSongPath());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getSerialNumber(), getName(), getDuration(), getGenre(), getArtistName());
+        return Objects.hash(getSerialNumber(), getName(), getDuration(), getGenre(), getArtistName(), getAlbum(), getSongPath());
     }
 }
