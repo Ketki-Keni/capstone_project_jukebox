d83d1214ca56e9b781b8cd93bdab1a70ee309801
Ketki Keni <sketki220@gmail.com>
2022-12-01 14:50:53 +0530

    completed displayAllSongs() method of the SongRepository class

--- src/main/java/com/niit/jdp/repository/SongRepository.java
+++ src/main/java/com/niit/jdp/repository/SongRepository.java
@@ -9,8 +9,12 @@ package com.niit.jdp.repository;
 import com.niit.jdp.model.Song;
 import com.niit.jdp.service.DatabaseService;
 import com.niit.jdp.service.MusicPlayerService;
-import com.sun.jdi.connect.spi.Connection;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SongRepository {
@@ -18,11 +22,31 @@ public class SongRepository {
     DatabaseService databaseService;
     MusicPlayerService musicPlayerService;
 
-    public SongRepository() {
+    public SongRepository() throws SQLException {
+        databaseService = new DatabaseService();
+        connection = databaseService.getConnection();
     }
 
     public List<Song> displayAllSongs() {
-        return null;
+        List<Song> songList = new ArrayList<>();
+        String selectQuery = "SELECT * FROM `jukebox`.`song`;";
+
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(selectQuery);
+            while (resultSet.next()) {
+                int serialNumber = resultSet.getInt("serial_number");
+                String name = resultSet.getString("name");
+                double duration = resultSet.getDouble("duration");
+                String genre = resultSet.getString("genre");
+                String artistName = resultSet.getString("artistName");
+                Song song = new Song(serialNumber, name, duration, genre, artistName);
+                songList.add(song);
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+
+        return songList;
     }
 
     public Song displaySongByName() {
