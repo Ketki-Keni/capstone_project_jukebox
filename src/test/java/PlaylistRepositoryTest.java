65b668a8b74f60acac7313eda4bec5d35ed043ee
Ketki Keni <sketki220@gmail.com>
2022-12-03 15:45:05 +0530

    added setUp() to the PlaylistRepositoryTest class

--- src/test/java/PlaylistRepositoryTest.java
+++ src/test/java/PlaylistRepositoryTest.java
@@ -6,11 +6,20 @@
 
 import com.niit.jdp.repository.SongRepository;
 import com.niit.jdp.service.DatabaseService;
+import org.junit.Before;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class PlaylistRepositoryTest {
     SongRepository songRepository;
     DatabaseService databaseService;
     Connection connection;
+
+    @Before
+    public void setUp() throws SQLException {
+        databaseService = new DatabaseService();
+        connection = databaseService.getConnection();
+        songRepository = new SongRepository();
+    }
 }
