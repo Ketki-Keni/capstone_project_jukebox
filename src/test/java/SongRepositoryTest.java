7332c9c391d39089b18df274ddd1592a2622bfa2
Ketki Keni <sketki220@gmail.com>
2022-12-03 14:30:36 +0530

    added givenSongsDatabaseThenReturnNumberOfSongRecords() to the SongRepositoryTest class

--- src/test/java/SongRepositoryTest.java
+++ src/test/java/SongRepositoryTest.java
@@ -4,18 +4,24 @@
  * Created with : IntelliJ IDEA Community Edition
  */
 
+import com.niit.jdp.model.Song;
 import com.niit.jdp.repository.SongRepository;
 import com.niit.jdp.service.DatabaseService;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class SongRepositoryTest {
     SongRepository songRepository;
     DatabaseService databaseService;
     Connection connection;
+    List<Song> songList;
 
     @Before
     public void setUp() throws SQLException {
@@ -31,5 +37,9 @@ public class SongRepositoryTest {
         connection = null;
     }
 
-
+    @Test
+    public void givenSongsDatabaseThenReturnNumberOfSongRecords() {
+        songList = songRepository.displayAllSongs();
+        assertEquals(9, songList.size());
+    }
 }
