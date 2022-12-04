74eeb79a34e777a756108b1ac024bddc96fcacec
Ketki Keni <sketki220@gmail.com>
2022-12-04 21:15:14 +0530

    created ArtistNameNotFoundException.java inside exception package

---
+++ src/main/java/com/niit/jdp/exception/ArtistNameNotFoundException.java
@@ -0,0 +1,10 @@
+/*
+ * Author : Ketki Keni
+ * Date : 04-12-2022
+ * Created with : IntelliJ IDEA Community Edition
+ */
+
+package com.niit.jdp.exception;
+
+public class ArtistNameNotFoundException {
+}
