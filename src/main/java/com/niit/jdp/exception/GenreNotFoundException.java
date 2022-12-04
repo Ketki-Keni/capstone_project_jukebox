2e7d9dd34c6e8cb69c02eaf7fbb93b09a361cb89
Ketki Keni <sketki220@gmail.com>
2022-12-04 20:57:35 +0530

    created GenreNotFoundException.java inside exception package

---
+++ src/main/java/com/niit/jdp/exception/GenreNotFoundException.java
@@ -0,0 +1,11 @@
+/*
+ * Author : Ketki Keni
+ * Date : 04-12-2022
+ * Created with : IntelliJ IDEA Community Edition
+ */
+
+package com.niit.jdp.exception;
+
+public class GenreNotFoundException extends Exception {
+
+}
