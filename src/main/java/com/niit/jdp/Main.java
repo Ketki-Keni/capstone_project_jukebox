a89e253b51b9e5304a0e882fdf89cf4dbd80ba6d
Ketki Keni <sketki220@gmail.com>
2022-12-05 09:29:08 +0530

    modified while condition inside main()

--- src/main/java/com/niit/jdp/Main.java
+++ src/main/java/com/niit/jdp/Main.java
@@ -119,7 +119,7 @@ public class Main {
                     default:
                         System.out.println("Invalid option selected");
                 }
-            } while (option != 0);
+            } while (option != 7);
         } catch (SQLException | SongNotFoundException | GenreNotFoundException |
                  ArtistNameNotFoundException exception) {
             exception.printStackTrace();
