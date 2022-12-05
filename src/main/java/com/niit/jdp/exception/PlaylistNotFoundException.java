bde9a3a6fd2169c44d421c24221df13b62db3860
Ketki Keni <sketki220@gmail.com>
2022-12-05 16:40:31 +0530

    created PlaylistNotFoundException.java inside exception package

---
+++ src/main/java/com/niit/jdp/exception/PlaylistNotFoundException.java
@@ -0,0 +1,10 @@
+/*
+ * Author : Ketki Keni
+ * Date : 05-12-2022
+ * Created with : IntelliJ IDEA Community Edition
+ */
+
+package com.niit.jdp.exception;
+
+public class PlaylistNotFoundException {
+}
